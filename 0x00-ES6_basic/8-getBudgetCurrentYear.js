aa8d1df5eece16df81eb5819596e8e5e36db0473
Siba <sibaquma@gmail.com>
2024-06-29 00:07:51 +0000

    updates

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,14 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-
-  const currentYear = getCurrentYear();
   const budget = {
-
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita
- };
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
   return budget;
 }
